990cdfff1f507c736af8d6747d3517eedb6cda84
Kiran Gurung <kg820532@gmail.com>
2022-04-13 12:47:09 +0100

    complete

--- requirements/03 - Local storage.md
+++ requirements/03 - Local storage.md
@@ -22,7 +22,7 @@ The important methods to know are:
 The user has logged in and now has a valid bearer token saved in local storage. Which header do we need to put this token into when requesting a protected resource from a server?
 
 Answer:
-
+authorization header
 ### Q2
 
 Imagine the below code gets profile information for a user by ID but requires a valid token to access. What would you add to include the token from local storage?
@@ -31,7 +31,8 @@ Imagine the below code gets profile information for a user by ID but requires a 
 fetch('http://localhost:4000/user-profile/1', {
     method: 'GET',
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'headers' : 'bearer ' + localStorage.getItem('token')
     }
 })
 ```
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,82 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 export default function App() {
-    return (
-        <div className="App">
 
+  const [register, setRegister] = useState(null);
+  const [login, setlogin] = useState(false);
+console.log(login)
+
+  const formRegister = (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    };
+
+    fetch("http://localhost:4000/register", options)
+      .then((res) => res.json())
+      .then((json) => setRegister(json));
+  };
+
+  const formLogin = (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    };
+
+    fetch("http://localhost:4000/login", options)
+      .then((res) => res.json())
+      .then((json) =>{ 
+        setlogin(json.verify)  
+        localStorage.setItem('token',json.token)});
+  };
+  return (
+    <>
+      <div className="App">
+        <h1>Register</h1>
+        <form className="form" onSubmit={(e) => formRegister(e)}>
+          <label for="username">Username: </label>
+          <input type="text" name="username" id="username-register" required></input>
+
+          <label for="password">Password :</label>
+          <input type="password" name="password" id="password-register" required></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+        <div>
+          {register !== null &&
+            `Thankyou for registering ${register.user.username}`}
         </div>
-    );
+
+        <h1>Login</h1>
+
+        <form className="form" onSubmit={(e) => formLogin(e)}>
+          <label for="username">Username: </label>
+          <input type="text" name="username" id="username-login" required></input>
+
+          <label for="password">Password :</label>
+          <input type="password" name="password" id="password-login" required></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+        <div>{login === true && `Welcomeback!`}</div>
+      </div>
+    </>
+  );
 }
